e9560f5a597982371de45014a9d9d0166a705abd
Kirill Markov <dev.sansagol@gmail.com>
2018-03-11 13:10:12 +0300

    Add the cyberfoxAPI project.

---
+++ CyberFoxApi/Models/AuthenticationResponseItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sansagol.CyberFox.CyberFoxApi.Models
+{
+    public class AuthenticationResponseItem
+    {
+        public long UserId { get; set; }
+        public string AccessToken { get; set; }
+    }
+}
